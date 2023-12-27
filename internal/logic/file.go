3b48832261761fb3738ce91c4d461c7a1e0c8670
ossan-dev <ivan.pesenti.dev@gmail.com>
2023-12-27 14:31:28 +0100

    review 2023-12-27

--- internal/logic/file.go
+++ internal/logic/file.go
@@ -17,36 +17,41 @@ import (
 func AppendToFile(urlCheck models.DomainStatus, split *bool, folderPath *string) {
 	var file *os.File
 	if !doesExist(*folderPath, "") {
-		os.Mkdir(*folderPath, 0700)
+		os.Mkdir(*folderPath, 0o700)
 	}
 	if *split {
 		defaultDailyFolder := time.Now().Format("2006_01_02")
 		if !doesExist(*folderPath+"/"+defaultDailyFolder, "") {
-			os.Mkdir(*folderPath+"/"+defaultDailyFolder, 0700)
+			os.Mkdir(*folderPath+"/"+defaultDailyFolder, 0o700)
 		}
-		filePath := *folderPath+"/"+defaultDailyFolder
+		// FIXME: try using filepath pkg of the Std Lib
+		filePath := *folderPath + "/" + defaultDailyFolder
 		var err error
+		// FIXME: extrapolate this logic into a function that accepts a string input param and return another string value, plus an error if something is wrong.
 		domainNoProtocol := strings.SplitAfter(urlCheck.Url, "www.")
 		justDomainName := strings.Split(domainNoProtocol[1], ".")
-		fileName := defaultDailyFolder+"_"+justDomainName[0]+".txt"
+		fileName := defaultDailyFolder + "_" + justDomainName[0] + ".txt"
 		if doesExist(filePath, fileName) {
-			fileName = strings.ReplaceAll(fileName, justDomainName[0] + ".txt", strings.ReplaceAll(domainNoProtocol[1], ".", "_") + ".txt")
+			fileName = strings.ReplaceAll(fileName, justDomainName[0]+".txt", strings.ReplaceAll(domainNoProtocol[1], ".", "_")+".txt")
 		}
 		file, err = os.OpenFile(filePath+"/"+fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644)
 		ErrorCheck(err)
 		defer file.Close()
 	} else {
 		var err error
+		// FIXME: add to the .gitignore every file called "url_list_status.txt" located anywhere
 		file, err = os.OpenFile(*folderPath+"/"+"url_list_status.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644)
 		ErrorCheck(err)
 		defer file.Close()
 	}
 	_, err := file.WriteString(urlCheck.Url + " " + urlCheck.Status + "\n")
 	ErrorCheck(err)
+	// [Q]: do you need this Sync method? If so, you have to check the error.
 	file.Sync()
 	PrintLog(urlCheck.Url + " - Checked and saved")
 }
 
+// BUG: avoid this func. If you encounter an error you should immediately STOP the execution. Otherwise, you can get a nil dereference error.
 func ErrorCheck(err error) {
 	if err != nil {
 		fmt.Println(err)
@@ -54,10 +59,11 @@ func ErrorCheck(err error) {
 }
 
 func PrintLog(msg string) {
-	fmt.Println(time.Now().Format("2006-01-02 15:04:05") + ":",msg)
+	fmt.Println(time.Now().Format("2006-01-02 15:04:05")+":", msg)
 }
 
 func GetFileNumLines(file *os.File) int {
+	// FIXME: not meaningful name
 	urlList := bufio.NewScanner(file)
 	count := 0
 	for urlList.Scan() {
@@ -65,25 +71,28 @@ func GetFileNumLines(file *os.File) int {
 			count++
 		}
 	}
+	// BUG: always checks for urlList.Err() and return it
 	_, err := file.Seek(0, io.SeekStart)
 	if err != nil {
+		// BUG: return the error to be handled above in the chain
 		log.Fatal(err)
 	}
 	return count
 }
 
 func doesExist(folderPath string, fileName string) bool {
-	path := folderPath+"/"+fileName
+	// FIXME: filepath pkg
+	path := folderPath + "/" + fileName
 	if fileName == "" {
 		path = folderPath
 	}
-	_ , error := os.Stat(path)
+	_, error := os.Stat(path)
 	if os.IsNotExist(error) {
-	  return false
+		return false
 	} else {
-	  return true
+		return true
 	}
-  }
+}
 
 func GetLocalPath() string {
 	_, b, _, _ := runtime.Caller(0)
--- internal/logic/url.go
+++ internal/logic/url.go
@@ -8,9 +8,10 @@ import (
 )
 
 func CheckStatusAsync(url string, ch chan models.DomainStatus) {
+	// FIXME: merge the two following lines into one
 	var urlCheck models.DomainStatus
 	urlCheck = CheckDomainStatus(url)
-	ch <-urlCheck
+	ch <- urlCheck
 }
 
 func CheckDomainStatus(url string) models.DomainStatus {
@@ -19,6 +20,7 @@ func CheckDomainStatus(url string) models.DomainStatus {
 	httpClient := http.Client{Timeout: 5 * time.Second}
 	response, err := httpClient.Get(url)
 	if err != nil {
+		// FIXME: maybe, it's better to report the status code returned by the hostname invoked. Should be 408
 		urlCheck.Status = "Timeout"
 		return urlCheck
 	}
