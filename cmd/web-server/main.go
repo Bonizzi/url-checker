57e06c183f9286905954be592e5f8fe2775979bb
ossan <ivan.pesenti.dev@gmail.com>
2024-01-26 17:58:15 +0100

    review 2024-01-26

--- cmd/web-server/main.go
+++ cmd/web-server/main.go
@@ -7,6 +7,13 @@ import (
 	"url-checker/cmd/web-server/routes"
 )
 
+// BUG: issue with the elapsed time.
+// To reproduce it, I changed this call in the "url.txt" file:
+// old: http://localhost:8000/slowdown?wait=5
+// new: http://localhost:8000/slowdown?wait=7
+// when I run it async, it gives me ~5s as elapsed time.
+// when I run this: curl -o /dev/null -s -w 'Total: %{time_total}s\n' http://localhost:8000/slowdown?wait=7
+// it gives me ~7s which is OK.
 func main() {
 	port := "8000"
 	log.Println("Starting server on port " + port)
